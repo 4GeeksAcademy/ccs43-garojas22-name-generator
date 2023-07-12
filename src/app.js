f85aa477de9339500fc3433613db123add9d0fa9
gabriel <130491279+garojas22@users.noreply.github.com>
2023-07-12 18:39:40 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generador(){
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let i = 0; i <= 10; i++) {
+
+    let pronunciado = ["the", "our"];
+
+      let adjetivo = ["great", "big"];
+      let sustantivo = ["jogger", "racoon"];
+      let direccion = [".com", ".net", ".us", ".io"];
+    
+      let random_pronunciado = Math.floor(Math.random() * pronunciado.length);
+      let random_adjetivo = Math.floor(Math.random() * adjetivo.length);
+      let random_sustantivo = Math.floor(Math.random() * sustantivo.length);
+      let random_direccion = Math.floor(Math.random() * direccion.length);
+
+
+      let resultado = ((pronunciado[random_pronunciado] + adjetivo[random_adjetivo] + sustantivo[random_sustantivo] + direccion[random_direccion]) );
+      console.log(resultado);
+
+  }
+}
+generador();
